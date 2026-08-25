4a5b1bd712454bd9f535eebdd19b1284b648d235
lpin0002 <workpinchy@gmail.com>
2026-08-25 13:41:57 +1000

    Adding some more ReadTheDocs files
     moving anything outside the 'docs' folder relating to documentaiton IN to the docs folder

--- docs/conf.py
+++ docs/conf.py
@@ -5,12 +5,6 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import os
-import sys
-# import sphinx_rtd_theme
-
-
-sys.path.insert(0, os.path.abspath('..'))
 
 project = 'BilbyFlow'
 copyright = '2026, Liam Pinchbeck'
@@ -20,27 +14,12 @@ release = '1.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-
-
-
-
-extensions = [
-    'sphinx.ext.autodoc',
+extensions = [    
     'nbsphinx',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.mathjax',
-    'myst_parser',
 ]
 
-
-
-master_doc = 'index'
-
-nbsphinx_execute = 'never'  # Set to 'never' if you want to use static notebooks
-
-
 templates_path = ['_templates']
-exclude_patterns = ['_build', '**.ipynb_checkpoints', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
@@ -48,20 +27,4 @@ exclude_patterns = ['_build', '**.ipynb_checkpoints', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-
 html_static_path = ['_static']
-
-source_suffix = '.rst'
-
-
-
-nbsphinx_prolog = """
-.. role:: raw-html(raw)
-   :format: html
-
-.. nbinfo::
-
-   This page is a tutorial for the (GammaBayes)[https://github.com/lpin0002/GammaBayes]
-   and the original notebook can be found within the docs/tutorials folder within the GitHub repository.
-
-"""
--- docs/index.rst
+++ docs/index.rst
@@ -1,22 +1,20 @@
-.. GammaBayes documentation master file, created by
-   sphinx-quickstart on Thu Oct 19 10:08:01 2023.
+.. BilbyFlow documentation master file, created by
+   sphinx-quickstart on Tue Aug 25 13:12:42 2026.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to BilbFlow's documentation!
-======================================
+Welcome to the BilbyFlow documentation!
+=======================================
+
+
+Bulk of documentations are built on top of jupter notebooks. 
+Some should be immediately runnable if you have the code installed, 
+and some will require you to have off-segment data accessable from your working directory.
+We will endeavour to make this clear within the relevant pages.
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
-   
+   :caption: Contents:  
+
    tutorials/QuickStart
 
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
